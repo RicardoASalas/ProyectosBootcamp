045bcc826de0e4df57e23a2431df72ae4fab8a08
Ricardo Salas <ricardevelopment@gmail.com>
2019-11-02 12:24:08 +0100

    Tercer commit, comenzado el proceso de enmaquetado, implementado la aparicion de las caratulas, aun no reproduce al arrastrar

--- proyecto1.SoundCloud/js/main.js
+++ proyecto1.SoundCloud/js/main.js
@@ -4,59 +4,66 @@ SC.initialize({
     client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
   });
 
-  //ESTA FUNCION SE INICIA AL HACER CLICK EN UNA CANCION, EL METODO JQUERY .HTML CREA UN CONTENEDOR CON EL REPRODUCTOR WIDGET DE SOUNCLOUD
+  //ESTA FUNCION SE INICIA AL HACER CLICK EN UNA CANCION, EL METODO JQUERY .HTML CREA UN CONTENEDOR EN EL 
+  //ELEMENTO FOOTER CON EL REPRODUCTOR WIDGET DE SOUNDCLOUD
   
-    function reproducirCancion(cancion){
-            var track_url = cancion;
-        SC.oEmbed(track_url, { auto_play: true })
+function reproducirCancion(cancion){
+    var track_url = cancion;
+    SC.oEmbed(track_url, { auto_play: false })
         .then(function(oEmbed) {
         const reproductorEmbebido=oEmbed.html;
-        $('footer').html(reproductorEmbebido);
-    })
-    .catch(e=>console.log(e));
+        $('#reproductor').html(reproductorEmbebido);
+        $('#reproductor iframe:first').addClass("sueltaCancion");
+        $('.sueltaCancion').droppable({
+            drop: function() {
+                console.log("entra aqui joder")
+               $('.sueltaCancion').attr("auto_play","true");
+               //const id =`"#${cancion}"`;
+               //$(id).remove();
+            }
+         });
+        })
+        .catch(e => console.log(e))
     
 } 
-
   
-  
-
-  
-
 async function gestionarBusqueda(){
     
     const valorDeInput =  $('input').val();
     await busqueda(valorDeInput)
-    .then( res => mostrarResultados(res))
-    .catch(e => console.log(e))
+        .then( res => mostrarResultados(res))
+        .catch(e => console.log(e))
     
-   
-    
-
 }
 
 function mostrarResultados(objetosCanciones){
+    
     console.log(objetosCanciones);
     const arrayObjetosRecibidos = objetosCanciones;
-    $('#listaReproduccion').html('');
-    arrayObjetosRecibidos.forEach(element => {
-        $('#listaReproduccion').append(`<a href="#" onclick="reproducirCancion(this.id)" id='${element.permalink_url}'>${element.title}</a>`);
-        console.log(element.title)
-        $('#fotoBanda').src=""
+    
+    jQuery(document).ready(function() {
+    
+    
+        $('#listaReproduccion').html('');
+        arrayObjetosRecibidos
+        .filter(element => element.artwork_url != null)
+        .forEach(element => {
+            console.log(element);
+            $('#listaReproduccion').append(`<div class = "arrastraCancion" ><a href="#" 
+            onclick="reproducirCancion(this.id)" id='${element.permalink_url}' class="titulo">
+            ${element.title}</a><img class="caratula" src='${element.artwork_url}'</div>`); 
+            $('.arrastraCancion').draggable({zIndex: 100, opacity: 0.35, containment: 'body', scroll: false});    
+      
+        });
     });
-    // objetosCanciones.array.forEach(element => {
-    //     $('#listaReproduccion').append(`<p id='${element.id}'>${element.title}</p>`);
-    // });
     
     
 }
-
-
-
 
 async function busqueda(parametroBusqueda){
     
     const resultadosBusqueda = await SC
-    .get('/tracks', {q: parametroBusqueda, limit:20})
+        .get('/tracks', {q: parametroBusqueda, limit: 100})
 
     return resultadosBusqueda;
      
