08d944919f16ffb8311c3d3a80cf8376d5e1608d
Ricardo Salas <ricardevelopment@gmail.com>
2019-11-02 17:19:54 +0100

    CUARTO COMMIT, IMPLEMETADA LA OPCION DE ARRASTRAR CANCIONES, YA LAS REPRODUCE. AUNQUE EN CHROME NO FUNCIONE LA REPRODUCCION AUTOMATICA, EN MOZILLA FIREFOX SI LO HACE. PRACTICAMENTE TERMINADO EL PROCESO DE MAQUETADO EN CSS3 Y LA ESTRUCTURA EN HTML5.

--- proyecto1.SoundCloud/js/main.js
+++ proyecto1.SoundCloud/js/main.js
@@ -4,62 +4,63 @@ SC.initialize({
     client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
   });
 
-  //ESTA FUNCION SE INICIA AL HACER CLICK EN UNA CANCION, EL METODO JQUERY .HTML CREA UN CONTENEDOR EN EL 
-  //ELEMENTO FOOTER CON EL REPRODUCTOR WIDGET DE SOUNDCLOUD
+//LA FUNCION DROP SE INICIA AL SALTAR EL EVENTO ONDROP DE LA CAJA CONTENEDORA DEL REPRODUCTOR, RECIBE DATOS DE LA 
+//FUNCION DRAG. EL METODO JQUERY .HTML CREA UN CONTENEDOR EN EL LA CAJA #REPRODUCTOR CON EL REPRODUCTOR WIDGET DE SOUNDCLOUD
   
-function reproducirCancion(cancion){
-    var track_url = cancion;
-    SC.oEmbed(track_url, { auto_play: false })
+function drop(event){
+   
+    event.preventDefault();
+    track_url = event.dataTransfer.getData('data');
+    zonaDeDrop = '<h2>Arrastra la cancion aqui para reproducirla.</h2>';
+    SC.oEmbed(track_url, { auto_play: true })
         .then(function(oEmbed) {
         const reproductorEmbebido=oEmbed.html;
         $('#reproductor').html(reproductorEmbebido);
-        $('#reproductor iframe:first').addClass("sueltaCancion");
-        $('.sueltaCancion').droppable({
-            drop: function() {
-                console.log("entra aqui joder")
-               $('.sueltaCancion').attr("auto_play","true");
-               //const id =`"#${cancion}"`;
-               //$(id).remove();
-            }
-         });
+        $('#reproductor').append(zonaDeDrop);
+    
         })
         .catch(e => console.log(e))
     
 } 
-  
-async function gestionarBusqueda(){
-    
-    const valorDeInput =  $('input').val();
-    await busqueda(valorDeInput)
-        .then( res => mostrarResultados(res))
-        .catch(e => console.log(e))
-    
+
+//FUNCION QUE EVITA QUE POR DEFECTO UN ELEMENTO NO PUEDA SOLTARSE SOBRE OTRO ELEMENTO
+function permitirDrop(event){
+
+    event.preventDefault();
 }
 
+//FUNCION QUE SE ACTIVA AL ARRASTRAR UN ELEMENTO CON LA PROPIEDAD ONDRAGSTART. MANDA DATOS A LA FUNCION DROP.
+function drag(event){
+
+    event.dataTransfer.setData('data', event.target.id);
+    
+}
+  
+
+//FUNCION QUE MUESTRA LOS RESULTADOS EN FORMA DE OBJETO OBTENIDOS DE LA FUNCION BUSQUEDA.
 function mostrarResultados(objetosCanciones){
     
-    console.log(objetosCanciones);
     const arrayObjetosRecibidos = objetosCanciones;
     
     jQuery(document).ready(function() {
-    
     
         $('#listaReproduccion').html('');
         arrayObjetosRecibidos
         .filter(element => element.artwork_url != null)
         .forEach(element => {
-            console.log(element);
-            $('#listaReproduccion').append(`<div class = "arrastraCancion" ><a href="#" 
-            onclick="reproducirCancion(this.id)" id='${element.permalink_url}' class="titulo">
-            ${element.title}</a><img class="caratula" src='${element.artwork_url}'</div>`); 
-            $('.arrastraCancion').draggable({zIndex: 100, opacity: 0.35, containment: 'body', scroll: false});    
+
+            $('#listaReproduccion').append(`<div id = '${element.permalink_url}' class = "arrastraCancion"  
+            draggable = "true" ondragstart = "drag(event)" ><p  class = "titulo">
+             ${element.title}</p><img class = "caratula" src='${element.artwork_url}'</div>`); 
+            
       
         });
     });
     
-    
 }
 
+//FUNCION QUE DE FORMA ASINCRONA HACE UNA CONSULTA A LA API DE SOUNDCLOUD RECOGIENDO LOS DATOS DE LA API QUE COINCIDAN
+//CON EL VALOR QUE HAYA INTRODUCIDO EL USUARIO EN EL ELEMENTO INPUT.
 async function busqueda(parametroBusqueda){
     
     const resultadosBusqueda = await SC
@@ -69,35 +70,19 @@ async function busqueda(parametroBusqueda){
      
 }
 
+//FUNCION ASINCRONA QUE GESTIONA LA BUSQUEDA PASANDO EL VALOR DEL ELEMENTO INPUT A LA FUNCION BUSQUEDA, QUE LA USARA PARA
+//ENCONTRAR CONCIDENCIAS EN LA API DE SOUNCLOUD Y EN CUANTO LAS RECIBA MANDARA EL OBJETO RECIBIDO A LA FUNCION MOSTRARRESULTADOS
+//PARA CREAR LA LISTA EN HTML.
+async function gestionarBusqueda(){
+    
+    const valorDeInput =  $('input').val();
+    await busqueda(valorDeInput)
+        .then( res => mostrarResultados(res))
+        .catch(e => console.log(e))
+    
+}
 
 
 
 
-// function Busqueda() {
-//      $('.lista').empty(); //Limpiamos la lista.
-//     var autor = $('input').val();
-//     SC.initialize({
-//       client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
-//     });
-//     SC.get('/tracks', {
-//       q: autor,
-//     }).then(function(tracks) {
-//       var numero = 0;
-//       if (tracks.length > 12) {
-//         numero = 12;
-//       } else {
-//         numero = tracks.length;
-//       }
-//       for (var i = 0; i < numero; i++) {
-//         if (tracks[i].artwork_url !== null) {
-//           $('.lista').append(
-//             "<div class='imagen_mini col-xs-2'><img src='" +
-//               tracks[i].artwork_url +
-//               "' id ='" +
-//               tracks[i].id +
-//               "' draggable='true' ondragstart='drag(event)'></div>"
-//           );
-//         }
-//       }
-//     });
-//    }
+
