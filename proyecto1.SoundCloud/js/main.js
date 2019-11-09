359eeace4c4b2682bc811d556216e49df46979bb
Ricardo Salas <ricardevelopment@gmail.com>
2019-11-09 14:41:31 +0100

    se ha creado una clase reproductor para hacer el codigo reutilizable en mayor o menor medida, añadida funcion de control de volumen, se ha habilitado el autoplay asi como el boton enter para las busquedas

--- proyecto1.SoundCloud/js/main.js
+++ proyecto1.SoundCloud/js/main.js
@@ -1,82 +1,61 @@
-
-//VALIDA EL ACCESO A LA API DE SOUND CLOUD
-SC.initialize({
-    client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
-  });
-
-//LA FUNCION DROP SE INICIA AL SALTAR EL EVENTO ONDROP DE LA CAJA CONTENEDORA DEL REPRODUCTOR, RECIBE DATOS DE LA 
-//FUNCION DRAG. EL METODO JQUERY .HTML CREA UN CONTENEDOR EN EL LA CAJA #REPRODUCTOR CON EL REPRODUCTOR WIDGET DE SOUNDCLOUD
-  
-async function drop(event){
-   
-    event.preventDefault();
-    track_url = event.dataTransfer.getData('data');
-    zonaDeDrop = '<h2>Arrastra la cancion aqui para reproducirla.</h2>';
-    await SC.oEmbed(track_url, { auto_play: true })
-        .then(function(oEmbed) {
-        const reproductorEmbebido=oEmbed.html;
-        $('#reproductor').html(reproductorEmbebido);
-        $('#reproductor').append(zonaDeDrop);
+$(document).ready(function () {
+    let token = 'aa06b0630e34d6055f9c6f8beb8e02eb'
+    let nombreReproductor = "Richify Player"
+    let cajaReproductor="reproductor";
+    let cajaListaReproduccion="listaReproduccion";
     
-        })
-        .catch(e => console.log(e))
+    //se instancia la clase reproductor y se crea un objeto de esa clase.
     
-} 
+    $('.introducirBusqueda').val("");
 
-//FUNCION QUE EVITA QUE POR DEFECTO UN ELEMENTO NO PUEDA SOLTARSE SOBRE OTRO ELEMENTO
-function permitirDrop(event){
-
-    event.preventDefault();
-}
-
-//FUNCION QUE SE ACTIVA AL ARRASTRAR UN ELEMENTO CON LA PROPIEDAD ONDRAGSTART. MANDA DATOS A LA FUNCION DROP.
-function drag(event){
-
-    event.dataTransfer.setData('data', event.target.id);
     
-}
-  
 
-//FUNCION QUE MUESTRA LOS RESULTADOS EN FORMA DE OBJETO OBTENIDOS DE LA FUNCION BUSQUEDA.
-function mostrarResultados(objetosCanciones){
-    
-    const arrayObjetosRecibidos = objetosCanciones;
-    
-        $('#listaReproduccion').html('');
-        arrayObjetosRecibidos
-        .filter(element => element.artwork_url != null)
-        .forEach(element => {
+    richify = new reproductor(nombreReproductor, token, 'richify' , " ", cajaReproductor, cajaListaReproduccion);
+    $('#tituloCabecera').append(nombreReproductor);
 
-            $('#listaReproduccion').append(`<div id = '${element.permalink_url}' class = "arrastraCancion"  
-            draggable = "true" ondragstart = "drag(event)" ><p  class = "titulo">
-             ${element.title}</p><img class = "caratula" draggable = "false" src='${element.artwork_url}'</div>`); 
+    richify.autentificarToken();
+    
+    
+    //SE CREA EL CONTENEDOR DEL REPRODUCTOR EMBEBIDO DE SOUND CLOUD y EL DE LA LISTA DE REPRODUCCION
+
+         $('main').append(
+        `<div id=${cajaListaReproduccion}>
+        </div>`);
+
+        $('main').append(
+        `<div id=${cajaReproductor} ondragover = "richify.permitirDrop(event)" ondrop = "richify.drop(event)">
+            <h2>Arrastra la cancion aqui para reproducirla.</h2>
+        </div>`
+        );
+    
+    
+    //FUNCION QUE GESTIONA LA BUSQUEDA PASANDO EL VALOR DEL ELEMENTO INPUT A LA FUNCION BUSQUEDA, QUE LA USARA PARA
+    //ENCONTRAR CONCIDENCIAS EN LA API DE SOUNCLOUD Y EN CUANTO LAS RECIBA MANDARA EL OBJETO RECIBIDO A LA FUNCION MOSTRARRESULTADOS
+    //PARA CREAR LA LISTA EN HTML.
+
+        async function gestionarBusqueda() {
             
+               
+                richify.parametroBusqueda = $('.introducirBusqueda').val();
+            
+                let objetoCanciones = await richify.busqueda();
+                
+    
+                richify.mostrarResultados(objetoCanciones)
+                
+        }
+    
+        $('#botonBuscar').click( gestionarBusqueda);
+    
+        $('.introducirBusqueda').on("keyup", e=>{
+            if (e.keyCode === 13){
+                
+                gestionarBusqueda();
+            }
         });
     
-}
 
-//FUNCION QUE DE FORMA ASINCRONA HACE UNA CONSULTA A LA API DE SOUNDCLOUD RECOGIENDO LOS DATOS DE LA API QUE COINCIDAN
-//CON EL VALOR QUE HAYA INTRODUCIDO EL USUARIO EN EL ELEMENTO INPUT.
-async function busqueda(parametroBusqueda){
-    
-    const resultadosBusqueda = await SC
-        .get('/tracks', {q: parametroBusqueda, limit: 100})
-
-    return resultadosBusqueda;
-     
-}
-
-//FUNCION ASINCRONA QUE GESTIONA LA BUSQUEDA PASANDO EL VALOR DEL ELEMENTO INPUT A LA FUNCION BUSQUEDA, QUE LA USARA PARA
-//ENCONTRAR CONCIDENCIAS EN LA API DE SOUNCLOUD Y EN CUANTO LAS RECIBA MANDARA EL OBJETO RECIBIDO A LA FUNCION MOSTRARRESULTADOS
-//PARA CREAR LA LISTA EN HTML.
-async function gestionarBusqueda(){
-    
-    const valorDeInput =  $('input').val();
-    await busqueda(valorDeInput)
-        .then( res => mostrarResultados(res))
-        .catch(e => console.log(e))
-    
-}
+})
 
 
 
---
+++ proyecto1.SoundCloud/js/reproductor.js
@@ -0,0 +1,114 @@
+class reproductor {
+
+    constructor(nomRep, token, nomObj, busc, cajaReproductor, cajaListaReproduccion) {
+
+        this.nombreReproductor = nomRep;
+        this.tokenAutentificacion = token;
+        this.nombreObjeto = nomObj;
+        this.parametroBusqueda = '';
+        this.arrayObjetosRecibidos = [];
+        this.cajaIdReproductor = cajaReproductor;
+        this.cajaIdListaReproduccion = cajaListaReproduccion;
+
+    }
+
+    async autentificarToken() {
+        //VALIDA EL ACCESO A LA API DE SOUND CLOUD
+        try {
+
+            await SC.initialize({
+                client_id: this.tokenAutentificacion
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }  
+
+    //FUNCION QUE MUESTRA LOS RESULTADOS EN FORMA DE OBJETO OBTENIDOS DE LA FUNCION BUSQUEDA.
+    mostrarResultados(objetosCanciones) {
+
+        let selectorIdListaReproduccion = `#${this.cajaIdListaReproduccion}`
+        //limpia la lista de reproduccion despues de cada busqueda
+        $(selectorIdListaReproduccion).html("");
+        this.arrayObjetosRecibidos = objetosCanciones;
+
+        //$(`'#${this.idCajaListaReproduccion}'`).html('');
+        this.arrayObjetosRecibidos
+            .filter(element => element.artwork_url != null)
+            .forEach(element => {
+
+                $(selectorIdListaReproduccion).append(`<div id = '${element.id}' class = "arrastraCancion" 
+                draggable = "true" ondragstart = '${this.nombreObjeto}.drag(event)' >
+                <img class = "caratula" draggable = "false" src='${element.artwork_url}'><div class="cajaDatosCancion">
+                <p  class = "datosCancion">
+                ${element.title}</p><p  class = "datosCancion">
+                ${element.user.username}</p></div></div>`);
+
+            });
+
+    }
+
+    //FUNCION QUE DE FORMA ASINCRONA HACE UNA CONSULTA A LA API DE SOUNDCLOUD RECOGIENDO LOS DATOS DE LA API QUE COINCIDAN
+    //CON EL VALOR QUE HAYA INTRODUCIDO EL USUARIO EN EL ELEMENTO INPUT.
+    async busqueda() {
+
+        let objeto = await SC
+            .get('/tracks', {
+                q: this.parametroBusqueda,
+                limit: 100
+            })
+            .then((res) => res)
+            .catch(e => console.log(e))
+
+        return objeto
+
+    }
+
+    //FUNCION QUE SE ACTIVA AL ARRASTRAR UN ELEMENTO CON LA PROPIEDAD ONDRAGSTART. MANDA DATOS A LA FUNCION DROP.
+    drag(event) {
+
+        event.dataTransfer.setData('data', event.target.id);
+
+    }
+    //FUNCION QUE EVITA QUE POR DEFECTO UN ELEMENTO NO PUEDA SOLTARSE SOBRE OTRO ELEMENTO
+    permitirDrop(event) {
+        event.preventDefault();
+    }
+    
+    controlarVolumen(event) {
+        
+        let widgetElemento = document.querySelector('iframe');
+
+        let reproductorWidget = SC.Widget(widgetElemento);
+        console.log(event);
+        let nivelVolumen = event.target.value;
+        console.log("el volumen es " + nivelVolumen);
+        reproductorWidget.setVolume(nivelVolumen);
+    }
+
+    //LA FUNCION DROP SE INICIA AL SALTAR EL EVENTO ONDROP DE LA CAJA CONTENEDORA DEL REPRODUCTOR, RECIBE DATOS DE LA 
+    //FUNCION DRAG. EL METODO JQUERY .HTML CREA UN CONTENEDOR EN EL LA CAJA #REPRODUCTOR CON EL REPRODUCTOR WIDGET DE SOUNDCLOUD
+
+    drop(event) {
+
+        event.preventDefault();
+
+        let cancionId = event.dataTransfer.getData('data');
+
+        let zonaDeDrop = '<h2>Arrastra la cancion aqui para reproducirla.</h2>';
+
+        let reproductorEmbebido = `<input id="controlVolumen" onchange="${this.nombreObjeto}.controlarVolumen(event)" type="range" min="0" max="100" value="50" step="10" id="mislider">
+        <span id="valor"></span>
+        <iframe allow="autoplay" width="100%" height="400" scrolling="no" frameborder="no" 
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${cancionId}?client_id=${this.tokenAutentificacion}visual=true&amp;
+        url=https://api.soundcloud.com/tracks/${cancionId}/stream;show_artwork=true&amp;auto_play=true">
+        </iframe>`;
+        let selectorCajaReproductor = `#${this.cajaIdReproductor}`;
+        $(selectorCajaReproductor).html(reproductorEmbebido);
+        $(selectorCajaReproductor).append(zonaDeDrop);
+
+    }
+
+    
+}
